8f1c1539918edeb711732ebcd340e6b63c5e4c57
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-18 22:36:12 -0700

    puppet module to create a file

---
+++ 0x0F-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Puppet module to create a file
+file { '/tmp/holberton':
+  ensure  	=> file,
+  path		=> '/tmp/holberton',
+  mode	  	=> "0774",
+  owner 	=> "www-data",
+  group 	=> "www-data",
+  content 	=> "I love Puppet",
+  }
