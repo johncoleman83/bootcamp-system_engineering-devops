ef58946ddc555395f9d6586212843dd183c8186c
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-18 22:50:15 -0700

    passed puppet-lint style

--- 0x0F-configuration_management/0-create_a_file.pp
+++ 0x0F-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Puppet manifest to create a file
 file { '/tmp/holberton':
-  ensure  	=> file,
-  path		=> '/tmp/holberton',
-  mode	  	=> "0774",
-  owner 	=> "www-data",
-  group 	=> "www-data",
-  content 	=> "I love Puppet",
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0774',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
   }
--- 0x0F-configuration_management/2-execute_a_command.pp
+++ 0x0F-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # puppet manifest to kill a process by process name
 exec { 'kill process':
-  command    => 'pkill -f killmenow',
-  path	     => '/usr/bin',
+  command => 'pkill -f killmenow',
+  path    => '/usr/bin',
 }
