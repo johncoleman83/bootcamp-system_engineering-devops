e97a3778a7f650ac6499b6b807a85a509709ac9c
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-18 14:23:26 -0700

    initializing new directory with file that makes API requests

---
+++ 0x16-api/0-gather_data_from_an_API.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+using https://jsonplaceholder.typicode.com/ REST API, for a given
+employee ID, returns information about his/her TODO list progress
+"""
+import sys
+import requests
+
+
+def make_request(data, num):
+    """
+    makes employee request and returns json dict response
+    """
+    root = 'https://jsonplaceholder.typicode.com'
+    url = root + data + num
+    return requests.get(url).json()
+
+
+def app():
+    """
+    returns info about employee todo list
+    """
+    num = str(sys.argv[1])
+    employee = make_request('/users/', num)
+    todos = make_request('/todos/?userId=', num)
+    completed = [t.get('title') for t in todos if t.get('completed')]
+    total = len(todos)
+    print('Employee {} is done with tasks ({}/{}):'.format(
+        employee.get('name'), len(completed), total))
+    for t in completed:
+        print('     {}'.format(t))
+
+
+if __name__ == '__main__':
+    """
+    MAIN App
+    """
+    app()
---
+++ 0x16-api/README.md
@@ -0,0 +1,38 @@
+# 0x16-api
+
+## Description
+
+This Repo has some of my (David John Coleman II) projects for Holberton School.
+The focus of these project is using python to run system admin (SRE) scripts,
+especially for working with API's
+
+
+## Environment
+
+* __OS:__ Ubuntu 14.04 LTS
+* __Language:__ python 3.4
+* __Style:__ pep8
+
+## Concepts Learned
+
+* What Bash scripting should not be used for
+* What is an API
+* What is a REST API
+* What are microservices
+* What is the CSV format
+* What is the JSON format
+* Python style guide:
+  * Package and module name style
+  * Class name style
+  * Variable name style
+  * Function name style
+  * Constant name style
+  * What is CapWords or CamelCase
+
+## Author
+
+David John Coleman II, [davidjohncoleman.com](http://www.davidjohncoleman.com/)
+
+## License
+
+MIT License
