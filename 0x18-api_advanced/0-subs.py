2c29943c35966dc315e23e114c13c26fad12452d
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-21 15:51:39 -0700

    API request to reddit for subreddit information

--- 0x18-api_advanced/0-subs.py
+++ 0x18-api_advanced/0-subs.py
@@ -1,5 +1,29 @@
 #!/usr/bin/python3
 """
-0-subs.py
+0-subs.py queries to https://www.reddit.com/dev/api/
 """
-def number_of_subscribers(subreddit)
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    returns the number of subscribers
+    """
+    domain = 'https://www.reddit.com'
+    path = '/r/{}/about.json'.format(subreddit)
+    url = '{}{}'.format(domain, path)
+    header = {
+        'user-agent': 'one-dope-boy',
+        'over18': 'yes'
+    }
+    response = requests.get(
+        url,
+        headers=header,
+        allow_redirects=False
+    )
+    code = response.status_code
+    if code >= 300:
+        return 0
+    data = response.json().get('data')
+    subscribers = data.get('subscribers')
+    return subscribers
