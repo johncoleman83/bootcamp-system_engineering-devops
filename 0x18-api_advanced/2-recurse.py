3d22ca429059f40fd197b1bf062f569d532351c9
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-21 16:29:10 -0700

    return all hot topics from subreddit using recursion, including count of hot_list as a parameter

--- 0x18-api_advanced/2-recurse.py
+++ 0x18-api_advanced/2-recurse.py
@@ -1,5 +1,45 @@
 #!/usr/bin/python3
 """
-2-recurse.py
+queries to https://www.reddit.com/dev/api/
 """
-def recurse(subreddit, hot_list=[])
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    returns the top ten hot posts for subreddit
+    """
+    domain = 'https://www.reddit.com'
+    path = '/r/{}/hot.json'.format(subreddit)
+    url = '{}{}'.format(domain, path)
+    if after:
+        payload = {
+            'after': after,
+            'count': str(len(hot_list))
+        }
+    else:
+        payload = {}
+    header = {
+        'user-agent': 'one-dope-boy',
+        'over18': 'yes'
+    }
+    response = requests.get(
+        url,
+        headers=header,
+        params=payload,
+        allow_redirects=False
+    )
+    code = response.status_code
+    if code >= 300:
+        return None
+    data = response.json().get('data')
+    after = data.get('after')
+    children = data.get('children')
+    for child in range(10):
+        hot_post = children[child].get('data')
+        title = hot_post.get('title')
+        hot_list.append(title)
+    if after:
+        return recurse(subreddit, hot_list, after)
+    else:
+        return hot_list
