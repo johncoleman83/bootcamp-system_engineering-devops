7b05641a14c56e93305f0927ee200ff83e2fca7a
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-21 16:10:35 -0700

    print top ten titles from hot articles

--- 0x18-api_advanced/1-top_ten.py
+++ 0x18-api_advanced/1-top_ten.py
@@ -1,5 +1,33 @@
 #!/usr/bin/python3
 """
-1-top_ten.py
+queries to https://www.reddit.com/dev/api/
 """
-def top_ten(subreddit)
+import requests
+
+
+def top_ten(subreddit):
+    """
+    returns the top ten hot posts for subreddit
+    """
+    domain = 'https://www.reddit.com'
+    path = '/r/{}/hot.json'.format(subreddit)
+    url = '{}{}'.format(domain, path)
+    header = {
+        'user-agent': 'one-dope-boy',
+        'over18': 'yes'
+    }
+    response = requests.get(
+        url,
+        headers=header,
+        allow_redirects=False
+    )
+    code = response.status_code
+    if code >= 300:
+        print('None')
+    else:
+        data = response.json().get('data')
+        children = data.get('children')
+        for child in range(10):
+            hot_post = children[child].get('data')
+            title = hot_post.get('title')
+            print('{}'.format(title))
