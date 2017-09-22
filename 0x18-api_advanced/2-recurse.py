b6dbab739389cadfb9c9ea4db6bae4078bf4e500
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-21 23:35:09 -0700

    added new function in the 2-recursion

--- 0x18-api_advanced/2-recurse.py
+++ 0x18-api_advanced/2-recurse.py
@@ -5,43 +5,51 @@ queries to https://www.reddit.com/dev/api/
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
+def make_get_request(subreddit, after, hot_list):
     """
-    returns the top ten hot posts for subreddit
+    makes get request to reddit API
     """
-    domain = 'https://www.reddit.com'
-    path = '/r/{}/hot.json'.format(subreddit)
-    url = '{}{}'.format(domain, path)
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     if after:
         payload = {
-            'after': after,
-            'count': str(len(hot_list)),
-            'limit': str(100)
+            'after': after, 'count': str(len(hot_list)), 'limit': '100'
         }
     else:
-        payload = {
-            'limit': str(100)
-        }
+        payload = {'limit': '100'}
     header = {
         'user-agent': 'one-dope-boy',
         'over18': 'yes'
     }
     response = requests.get(
-        url,
-        headers=header,
-        params=payload,
-        allow_redirects=False
+        url, headers=header,
+        params=payload, allow_redirects=False
     )
+    return response
+
+
+def add_titles(hot_list, children):
+    """
+    adds new titles to hottlist
+    """
+    for child in children:
+        hot_post = child.get('data')
+        title = hot_post.get('title')
+        hot_list.append(title)
+    return hot_list
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    returns the top ten hot posts for subreddit
+    """
+    response = make_get_request(subreddit, after, hot_list)
     code = response.status_code
     if code >= 300:
         return None
     data = response.json().get('data')
     after = data.get('after')
     children = data.get('children')
-    for child in children:
-        hot_post = child.get('data')
-        title = hot_post.get('title')
-        hot_list.append(title)
+    hot_list = add_titles(hot_list, children)
     if after:
         return recurse(subreddit, hot_list, after)
     else:
