f3a47d09fbaf6e914bec140a4d6bdff87b529f8b
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-21 19:58:03 -0700

    finds words count from all titles in API request to REddit Hot Topics of given subreddit

--- 0x18-api_advanced/100-count.py
+++ 0x18-api_advanced/100-count.py
@@ -1,5 +1,58 @@
 #!/usr/bin/python3
 """
-100-count.py
+queries to https://www.reddit.com/dev/api/
 """
-def count_words(subreddit, word_list)
+import requests
+
+
+def count_words(subreddit, word_list, after=None):
+    """
+    returns the top ten hot posts for subreddit
+    """
+    if type(word_list).__name__ == 'list':
+        word_list = {
+            word: 0 for word in word_list
+        }
+    domain = 'https://www.reddit.com'
+    path = '/r/{}/hot.json'.format(subreddit)
+    url = '{}{}'.format(domain, path)
+    if after:
+        payload = {
+            'after': after,
+            'limit': str(100)
+        }
+    else:
+        payload = {
+            'limit': str(100)
+        }
+    header = {
+        'user-agent': 'one-dope-boy',
+        'over18': 'yes'
+    }
+    response = requests.get(
+        url,
+        headers=header,
+        params=payload,
+        allow_redirects=False
+    )
+    code = response.status_code
+    if code >= 300:
+        return
+    data = response.json().get('data')
+    after = data.get('after')
+    children = data.get('children')
+    for child in children:
+        hot_post = child.get('data')
+        title = hot_post.get('title').split()
+        for word in word_list:
+            word_list[word] += title.count(word)
+    if after:
+        return count_words(subreddit, word_list, after)
+    else:
+        words_counts = [
+            (word, count) for word, count in word_list.items() if count > 0
+        ]
+        words_counts = sorted(words_counts, key=lambda x: x[1])
+        for word in words_counts:
+            print('{}: {}'.format(word[0], word[1]))
+        return
